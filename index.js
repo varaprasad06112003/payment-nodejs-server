a2ed41c9819ce4036e474deda615d79da76d7b4f
varaprasad06112003 <116376144+varaprasad06112003@users.noreply.github.com>
2024-10-08 13:32:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ app.post('/create-checkout-session', async(req, res) => {
             payment_method_types: ['card'],
             line_items,
             mode: 'payment',
-            success_url: "http://localhost:5173/success",
-            cancel_url: "http://localhost:5173/cancel",
+            success_url: "https://ezfoods.netlify.app/success",
+            cancel_url: "https://ezfoods.netlify.app/cancel",
         });
         res.json({id: session.id});
     } catch(error){
